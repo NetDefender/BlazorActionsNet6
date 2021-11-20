03a4b0d50a053a5874ab10fa37772b938a3d5afd
Daniel <dgarciam@oesia.com>
2021-11-20 20:34:30 +0100

    Ants-Blazor

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddAntDesign();
 
 await builder.Build().RunAsync();
-//Texto para poder hacer push
-//Otra línea
