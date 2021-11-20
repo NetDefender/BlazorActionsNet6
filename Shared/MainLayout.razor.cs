28f3986435456eaf8d71ba9a05dae4e91d2e7206
Daniel <dgarciam@oesia.com>
2021-11-20 22:24:22 +0100

    Definition lateral menu

---
+++ Shared/MainLayout.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorActionsNet6.Shared
+{
+    public sealed partial class MainLayout
+    {
+        #region fields
+        private bool _collapsed;
+        #endregion
+
+        #region methods
+        private void OnCollapse(bool collapsed)
+        {
+            _collapsed = collapsed;
+        }
+        #endregion
+    }
+}
